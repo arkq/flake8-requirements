c59ff280f914d5d353e32b79ac6e11e0fc94132e
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-09-29 19:18:08 +0300

    Renaming of path variable to be more descriptive

--- src/flake8_requirements/checker.py
+++ src/flake8_requirements/checker.py
@@ -357,7 +357,7 @@ class Flake8Checker(object):
             root_dir = os.path.abspath(os.path.join(root_dir, ".."))
 
     @classmethod
-    def resolve_requirement(cls, requirement, max_depth=0, path=None):
+    def resolve_requirement(cls, requirement, max_depth=0, parent_path=None):
         """Resolves flags like -r in an individual requirement line."""
 
         option = None
@@ -377,13 +377,15 @@ class Flake8Checker(object):
                 raise RuntimeError(msg.format(requirement))
             resolved = []
             # Error out if requirements file cannot be opened.
-            if not path:
-                path = cls.root_dir
-            with open(os.path.join(path, requirement)) as f:
+            if not parent_path:
+                parent_path = cls.root_dir
+            with open(os.path.join(parent_path, requirement)) as f:
                 for line in joinlines(f.readlines()):
                     resolved.extend(cls.resolve_requirement(
-                        line, max_depth - 1, os.path.join(cls.root_dir,
-                                                          os.path.dirname(requirement))))
+                        line,
+                        max_depth - 1,
+                        os.path.join(cls.root_dir,
+                                     os.path.dirname(requirement))))
             return resolved
 
         if option:
