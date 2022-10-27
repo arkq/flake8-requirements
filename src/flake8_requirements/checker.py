5f04533add939a1214768d56c549c35f5dba8af0
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2022-10-27 17:21:09 +0200

    Bump version

--- src/flake8_requirements/checker.py
+++ src/flake8_requirements/checker.py
@@ -18,7 +18,7 @@ from .modules import STDLIB_PY2
 from .modules import STDLIB_PY3
 
 # NOTE: Changing this number will alter package version as well.
-__version__ = "1.7.2"
+__version__ = "1.7.3"
 __license__ = "MIT"
 
 LOG = getLogger('flake8.plugin.requirements')
