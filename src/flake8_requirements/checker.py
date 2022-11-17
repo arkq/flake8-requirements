c5345a0c7bc868c86b45ece580d04851c3481a59
Markus Hofbauer <markus.hofbauer@tum.de>
2022-11-17 18:35:06 +0100

    Add opencv-python, hydra, ffmpeg to known modules
    https://pypi.org/project/opencv-contrib-python/ is a pre-built CPU-only
    OpenCV packages for Python
    
    https://hydra.cc/ is a framework for elegantly configuring complex
    applications

--- src/flake8_requirements/checker.py
+++ src/flake8_requirements/checker.py
@@ -18,7 +18,7 @@ from .modules import STDLIB_PY2
 from .modules import STDLIB_PY3
 
 # NOTE: Changing this number will alter package version as well.
-__version__ = "1.7.4"
+__version__ = "1.7.5"
 __license__ = "MIT"
 
 LOG = getLogger('flake8.plugin.requirements')
